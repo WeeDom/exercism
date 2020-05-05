d09652a088cf948b8a0a91456e3b5588f1c4ff44
Dominic Pain <dominic_pain@hotmail.com>
2020-05-05 12:27:56 +0100

    remove useless 0 from slice

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,10 @@
 def latest(scores):
     return scores[-1]
 
+
 def personal_best(scores):
-    scores.sort()
-    return scores[-1]
+    return sorted(scores)[-1]
+
 
 def personal_top_three(scores):
-    scores.sort(reverse=True)
-    return scores[0:3]
+    return sorted(scores, reverse=True)[:3]
